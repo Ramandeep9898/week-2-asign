108bf4cdf95f414c6252c553cf656d58af14a527
Ramandeep9898 <ramank6238299@gmail.com>
2022-01-25 16:09:49 +0530

    badge

---
+++ badge/main.js
@@ -0,0 +1,9 @@
+var hideBtn = document.querySelector(".hide-btn")
+var badge = document.querySelector(".badge1")
+hideBtn.addEventListener("click",()=>{
+    if(badge.style.display = "block"){
+        badge.style.display ="none"
+    }else if(badge.style.display = "none"){
+        console.log(badge)
+    }
+})
